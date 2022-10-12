96b878d929a0d26fd62a6211106183d4af2d2a33
thesaddest <youngscarlxrd@yandex.by>
2022-10-12 21:22:54 +0200

    add: possible fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,59 +4,6 @@ import axios from "axios";
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-    // const [name, setName] = useState("");
-    // const [surName, setSurName] = useState("");
-    // const [telephone, setTelephone] = useState("");
-    // const [instagram, setInstagram] = useState("");
-    // const {tg} = useTelegram();
-    //
-    // const onSendData = useCallback(() => {
-    //     const data = {
-    //         name,
-    //         surName,
-    //         telephone,
-    //     }
-    //
-    //     tg.sendData(JSON.stringify(data));
-    // }, [name, surName, telephone, tg]);
-    //
-    // useEffect(() => {
-    //     tg.onEvent("mainButtonClicked", onSendData);
-    //     return () => {
-    //         tg.offEvent("mainButtonClicked", onSendData);
-    //     }
-    // },[tg, onSendData]);
-    //
-    // useEffect(() => {
-    //     tg.MainButton.setParams({
-    //         text: "Отправить данные"
-    //     });
-    // },[tg.MainButton]);
-    //
-    // useEffect(() => {
-    //     if(!name || !surName || !telephone) {
-    //         tg.MainButton.hide();
-    //     } else {
-    //         tg.MainButton.show();
-    //     }
-    // }, [name, surName, telephone, tg.MainButton]);
-    //
-    // // const onChangeName = (e) => {
-    // //     setName(e.target.value);
-    // // }
-    //
-    // const onChangeSurName = (e) => {
-    //     setSurName(e.target.value);
-    // }
-    //
-    // const onChangeTelephone = (e) => {
-    //     setTelephone(e.target.value);
-    // }
-    //
-    // const onChangeInstargam = (e) => {
-    //     setInstagram(e.target.value);
-    // }
-
     const [status, setStatus] = useState({
         submitted: false,
         submitting: false,
@@ -144,9 +91,9 @@ const Form = () => {
     }, [inputs, tg]);
 
     useEffect(() => {
-        tg.onEvent("mainButtonClicked", onSendData, handleSubmit);
+        tg.onEvent("mainButtonClicked", onSendData).then(handleSubmit);
         return () => {
-            tg.offEvent("mainButtonClicked", onSendData, handleSubmit);
+            tg.offEvent("mainButtonClicked", onSendData);
         }
     },[tg, onSendData, handleSubmit]);
 
@@ -164,11 +111,15 @@ const Form = () => {
         }
     }, [inputs, tg.MainButton]);
 
+    useEffect(() => {
+        console.log(status)
+    }, [status])
+
     return (
         <div className={"form-wrapper"}>
             <div className={"form-container"}>
                 <h3 className={"text-title"}>Введите ваши данные</h3>
-                <form className={"form"} onSubmit={handleSubmit}>
+                <form className={"form"}>
                     {status.info.error && (
                         <div role={"alert"} className={"error-form"}>
                             <strong>Ошибка</strong>
@@ -216,27 +167,6 @@ const Form = () => {
                             />
                         </>
                     )}
-                    {/*<input*/}
-                    {/*    className={"input"}*/}
-                    {/*    type="text"*/}
-                    {/*    placeholder={"Фамилия"}*/}
-                    {/*    value={surName}*/}
-                    {/*    onChange={onChangeSurName}*/}
-                    {/*/>*/}
-                    {/*<input*/}
-                    {/*    className={"input"}*/}
-                    {/*    type="text"*/}
-                    {/*    placeholder={"Телефон (требуется для подтверждения заказа)"}*/}
-                    {/*    value={telephone}*/}
-                    {/*    onChange={onChangeTelephone}*/}
-                    {/*/>*/}
-                    {/*<input*/}
-                    {/*    className={"input"}*/}
-                    {/*    type="text"*/}
-                    {/*    placeholder={"Никнейм Instagram (опционально)"}*/}
-                    {/*    value={instagram}*/}
-                    {/*    onChange={onChangeInstargam}*/}
-                    {/*/>*/}
                 </form>
             </div>
         </div>
