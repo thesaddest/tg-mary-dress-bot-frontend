5fa74d574489ccba2e33a0eaecd1d390813830dc
thesaddest <youngscarlxrd@yandex.by>
2022-10-12 19:37:56 +0200

    test: formspree

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,61 +1,60 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import "./Form.css";
-import {useTelegram} from "../../hooks/useTelegram";
 import axios from "axios";
 
 const Form = () => {
-    const [name, setName] = useState("");
-    const [surName, setSurName] = useState("");
-    const [telephone, setTelephone] = useState("");
-    const [instagram, setInstagram] = useState("");
-    const {tg} = useTelegram();
-
-    const onSendData = useCallback(() => {
-        const data = {
-            name,
-            surName,
-            telephone,
-        }
-
-        tg.sendData(JSON.stringify(data));
-    }, [name, surName, telephone, tg]);
-
-    useEffect(() => {
-        tg.onEvent("mainButtonClicked", onSendData);
-        return () => {
-            tg.offEvent("mainButtonClicked", onSendData);
-        }
-    },[tg, onSendData]);
-
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: "Отправить данные"
-        });
-    },[tg.MainButton]);
-
-    useEffect(() => {
-        if(!name || !surName || !telephone) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [name, surName, telephone, tg.MainButton]);
-
-    const onChangeName = (e) => {
-        setName(e.target.value);
-    }
-
-    const onChangeSurName = (e) => {
-        setSurName(e.target.value);
-    }
-
-    const onChangeTelephone = (e) => {
-        setTelephone(e.target.value);
-    }
-
-    const onChangeInstargam = (e) => {
-        setInstagram(e.target.value);
-    }
+    // const [name, setName] = useState("");
+    // const [surName, setSurName] = useState("");
+    // const [telephone, setTelephone] = useState("");
+    // const [instagram, setInstagram] = useState("");
+    // const {tg} = useTelegram();
+    //
+    // const onSendData = useCallback(() => {
+    //     const data = {
+    //         name,
+    //         surName,
+    //         telephone,
+    //     }
+    //
+    //     tg.sendData(JSON.stringify(data));
+    // }, [name, surName, telephone, tg]);
+    //
+    // useEffect(() => {
+    //     tg.onEvent("mainButtonClicked", onSendData);
+    //     return () => {
+    //         tg.offEvent("mainButtonClicked", onSendData);
+    //     }
+    // },[tg, onSendData]);
+    //
+    // useEffect(() => {
+    //     tg.MainButton.setParams({
+    //         text: "Отправить данные"
+    //     });
+    // },[tg.MainButton]);
+    //
+    // useEffect(() => {
+    //     if(!name || !surName || !telephone) {
+    //         tg.MainButton.hide();
+    //     } else {
+    //         tg.MainButton.show();
+    //     }
+    // }, [name, surName, telephone, tg.MainButton]);
+    //
+    // // const onChangeName = (e) => {
+    // //     setName(e.target.value);
+    // // }
+    //
+    // const onChangeSurName = (e) => {
+    //     setSurName(e.target.value);
+    // }
+    //
+    // const onChangeTelephone = (e) => {
+    //     setTelephone(e.target.value);
+    // }
+    //
+    // const onChangeInstargam = (e) => {
+    //     setInstagram(e.target.value);
+    // }
 
     const [status, setStatus] = useState({
         submitted: false,
@@ -99,7 +98,7 @@ const Form = () => {
                 },
             });
             setInputs({
-                custmerName: "",
+                customerName: "",
                 email: "",
                 message: "",
             });
@@ -163,27 +162,27 @@ const Form = () => {
                             />
                         </>
                     )}
-                    <input
-                        className={"input"}
-                        type="text"
-                        placeholder={"Фамилия"}
-                        value={surName}
-                        onChange={onChangeSurName}
-                    />
-                    <input
-                        className={"input"}
-                        type="text"
-                        placeholder={"Телефон (требуется для подтверждения заказа)"}
-                        value={telephone}
-                        onChange={onChangeTelephone}
-                    />
-                    <input
-                        className={"input"}
-                        type="text"
-                        placeholder={"Никнейм Instagram (опционально)"}
-                        value={instagram}
-                        onChange={onChangeInstargam}
-                    />
+                    {/*<input*/}
+                    {/*    className={"input"}*/}
+                    {/*    type="text"*/}
+                    {/*    placeholder={"Фамилия"}*/}
+                    {/*    value={surName}*/}
+                    {/*    onChange={onChangeSurName}*/}
+                    {/*/>*/}
+                    {/*<input*/}
+                    {/*    className={"input"}*/}
+                    {/*    type="text"*/}
+                    {/*    placeholder={"Телефон (требуется для подтверждения заказа)"}*/}
+                    {/*    value={telephone}*/}
+                    {/*    onChange={onChangeTelephone}*/}
+                    {/*/>*/}
+                    {/*<input*/}
+                    {/*    className={"input"}*/}
+                    {/*    type="text"*/}
+                    {/*    placeholder={"Никнейм Instagram (опционально)"}*/}
+                    {/*    value={instagram}*/}
+                    {/*    onChange={onChangeInstargam}*/}
+                    {/*/>*/}
                 </form>
             </div>
         </div>
